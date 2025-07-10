f6dbcaa8cb522b04258407486ded8db2262391c5
mahatab6 <mahatabcall56@gmail.com>
2025-07-10 11:38:11 +0600

    server side new post added to user

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const mongoose = require("mongoose");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
 const app = express();
@@ -13,12 +13,50 @@ app.use(express.json());
 
 
 
+const uri = process.env.MongoDB_URL;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const database = client.db("NodeTalkDataBase");
+    const postCollection = database.collection("postColl")
+
+    app.post('/add-user-post', async(req, res) => {
+      const newPost = req.body;
+      const result = await postCollection.insertOne(newPost);
+      res.send(result)
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
 app.get('/', (req, res) => {
-  res.send('Pet Management Backend is Running');
+  res.send('NodeTalk Backend is Running');
 });
 
 app.listen(port, () => {
-  console.log(`Pet Management app listening on port ${port}`)
+  console.log(`NodeTalk app listening on port ${port}`)
 })
 
 
